b2bfc21d86d52afa328a939b00d0e6bc48376e08
Chris Csekey <chrisjc25@gmail.com>
2017-08-27 21:02:57 -0700

    Apply center-div class

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import EnigmaCard from './EnigmaCard'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='center-div'>
         <EnigmaCard />
       </div>
     );
